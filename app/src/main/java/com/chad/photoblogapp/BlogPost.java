da69c2d409d34b7ae8d02e04a44c60fd974722fa
keithchad <63049827+keithchad@users.noreply.github.com>
2020-07-08 23:17:04 +0300

    Blog Post

--- app/src/main/java/com/chad/photoblogapp/BlogPost.java
+++ app/src/main/java/com/chad/photoblogapp/BlogPost.java
@@ -4,7 +4,59 @@ import java.sql.Timestamp;
 
 public class BlogPost {
 
-    public String user_id, image_url, desc, image_thumb;
     public Timestamp timeStamp;
+    public String user_id, image_url, desc, image_thumb;
+
+    public BlogPost() {}
+
+    public Timestamp getTimeStamp() {
+        return timeStamp;
+    }
+    public BlogPost(String user_id, String image_url, String desc, String image_thumb, Timestamp timeStamp) {
+        this.user_id = user_id;
+        this.image_url = image_url;
+        this.desc = desc;
+        this.image_thumb = image_thumb;
+        this.timeStamp = timeStamp;
+    }
+
+    public void setTimeStamp(Timestamp timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getImage_thumb() {
+        return image_thumb;
+    }
+
+    public void setImage_thumb(String image_thumb) {
+        this.image_thumb = image_thumb;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+
 
 }
