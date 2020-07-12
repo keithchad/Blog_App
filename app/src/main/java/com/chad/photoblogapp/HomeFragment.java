9aab26fab2701a9721924588c4c65c3a886f327a
keithchad <63049827+keithchad@users.noreply.github.com>
2020-07-12 21:46:33 +0300

    Querying data

--- app/src/main/java/com/chad/photoblogapp/HomeFragment.java
+++ app/src/main/java/com/chad/photoblogapp/HomeFragment.java
@@ -2,6 +2,7 @@ package com.chad.photoblogapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
@@ -43,7 +45,7 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
@@ -57,6 +59,26 @@ public class HomeFragment extends Fragment {
 
         firebaseFirestore = FirebaseFirestore.getInstance();
 
+        blog_list_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                Boolean reachedBottom  = !recyclerView.canScrollVertically(1);
+                if(reachedBottom) {
+
+                    String desc = lastVisible.getString("desc");
+
+
+                    Toast.makeText(container.getContext(), "Reached :" + desc, Toast.LENGTH_SHORT).show();
+
+                    loadMorePost();
+
+                }
+
+            }
+        });
+
         Query firstQuery = firebaseFirestore.collection("Posts")
                 .orderBy("timestamp", Query.Direction.DESCENDING)
                 .limit(3);
@@ -64,6 +86,8 @@ public class HomeFragment extends Fragment {
         firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                lastVisible = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() -1);
 
                 for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
                     if (doc.getType() == DocumentChange.Type.ADDED) {
@@ -84,25 +108,34 @@ public class HomeFragment extends Fragment {
 
         Query nextQuery = firebaseFirestore.collection("Posts")
                 .orderBy("timestamp", Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
                 .limit(3);
 
         nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
 
-                for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
-                    if (doc.getType() == DocumentChange.Type.ADDED) {
+                if (!queryDocumentSnapshots.isEmpty()) {
 
-                        BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
-                        blog_list.add(blogPost);
 
-                        blogRecyclerAdapter.notifyDataSetChanged();
+                    lastVisible = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() - 1);
 
+                    for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                            BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                            blog_list.add(blogPost);
+
+                            blogRecyclerAdapter.notifyDataSetChanged();
+
+                        }
                     }
+
                 }
             }
         });
 
     }
+
 
 }
